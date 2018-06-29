d931683d153ecaddf21f59e86f48b2fd51c11f26
AbnerGarcia96 <abnerjosgarfer@yahoo.com>
2018-06-28 22:58:04 -0600

    test data

--- tensorflow/data/file_rename.py
+++ tensorflow/data/file_rename.py
@@ -1,9 +1,9 @@
 import os
 
-for folder in os.listdir('train/'):
+for folder in os.listdir('test/'):
     print("Leyendo "+folder)
     file_counter = 0
-    for file in os.listdir('train/'+folder):
-        os.rename('train/'+folder+'/'+file, 'train/'+folder+'/'+folder+'-'+str(file_counter)+'.png')
+    for file in os.listdir('test/'+folder):
+        os.rename('test/'+folder+'/'+file, 'test/'+folder+'/'+folder+'-'+str(file_counter)+'.png')
         file_counter = file_counter + 1
     print(folder+" Completado")
--- tensorflow/data/image_resize.py
+++ tensorflow/data/image_resize.py
@@ -1,14 +1,14 @@
 import os
 from PIL import Image
 
-for folder in os.listdir('train/'):
+for folder in os.listdir('test/'):
     print("Leyendo "+folder)
     file_counter = 0
-    for file in os.listdir('train/'+folder):
+    for file in os.listdir('test/'+folder):
         #resize
-        image = Image.open('train/'+folder+'/'+file)
+        image = Image.open('test/'+folder+'/'+file)
         out = image.resize((80, 80))
-        out.save('train/'+folder+'/'+folder+'-'+str(file_counter)+'.png')
+        out.save('test/'+folder+'/'+folder+'-'+str(file_counter)+'.png')
         file_counter = file_counter + 1
 
     print(folder+" Completado")
