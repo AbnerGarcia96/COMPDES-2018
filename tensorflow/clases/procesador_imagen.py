7d1c01e412ea15da124190bb2df0d39e0146a809
AbnerGarcia96 <abnerjosgarfer@yahoo.com>
2018-06-29 02:29:32 -0600

    Procesar imagen

--- tensorflow/clases/procesador_imagen.py
+++ tensorflow/clases/procesador_imagen.py
@@ -1,5 +1,7 @@
 import os
 from PIL import Image
+import scipy.misc as smp
+import numpy as np
 
 class ProcesadorImagen:
     directorio = ''
@@ -26,7 +28,7 @@ class ProcesadorImagen:
             print(folder+' Completado \n')
 
     def redimensionarImagenes(self):
-        print('=================================Redimensionando imagenes=================================')
+        print('================================= Redimensionando imagenes =================================')
         for folder in os.listdir(self.directorio+'/'):
             print('Leyendo '+folder+'...')
             contador = 0
@@ -36,3 +38,29 @@ class ProcesadorImagen:
                 out.save(self.directorio+'/'+folder+'/'+folder+'-'+str(contador)+'.png')
                 contador = contador + 1
             print(folder+' Completado \n')
+
+    def procesarImagen(self):
+        print('================================= Procesando imagenes =================================')
+        data = []
+        for folder in os.listdir(self.directorio+'/'):
+            print('Leyendo '+folder+'...')
+            for archivo in os.listdir(self.directorio+'/'+folder):
+                # print('Leyendo '+archivo+'...')
+                imgData = []
+                img = Image.open(self.directorio+'/'+folder+'/'+archivo)
+                pixeles = np.array(img.getdata())
+                imgGris = list(self.escalaDeGrises(pixel) for pixel in pixeles)
+                img.putdata(imgGris)
+                for _pixel in imgGris:
+                    imgData.append(self.getValorRGB(_pixel))
+                data.append(imgData)
+        data = np.array(data)
+        print(data.shape)
+
+    def escalaDeGrises(self, rgb):
+        valor = int((rgb[0]+rgb[1]+rgb[2])/3)
+        return (valor, valor, valor)
+
+    def getValorRGB(self, rgb):
+        valor = int(abs(((rgb[0]+rgb[1]+rgb[2])/765 - 1)))
+        return valor
--- tensorflow/main.py
+++ tensorflow/main.py
@@ -4,7 +4,8 @@ from clases.procesador_imagen import ProcesadorImagen
 def main():
     if(len(sys.argv) == 2):
         directorio = sys.argv[1]
-        img = ProcesadorImagen(directorio, False, True)
+        img = ProcesadorImagen(directorio, False, False)
+        img.procesarImagen()
     else:
         print("Faltan argumentos: python3 main.py [directorio]")
     
